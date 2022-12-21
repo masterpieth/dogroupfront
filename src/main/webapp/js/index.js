4a779a8d7a3ea5194e3d46c2d69e949b5370f4de
NYK <kny0339@gmail.com>
2022-12-21 19:16:11 +0900

    2022-12-21 김나영 스터디 개설 작업중

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,7 +1,8 @@
 $(function(){
     showMenu()
-
+    logoutEvent()
     let queryStr = location.search.substr(1).split('=')
+    console.log(queryStr)
     switch(queryStr[0]) {
         case 'status':
             alert('탈퇴된 회원입니다. 다른 계정으로 가입해주세요.')
@@ -9,21 +10,27 @@ $(function(){
         case 'email':
             localStorage.setItem('loginedId', queryStr[1])
             history.replaceState({}, null, location.pathname)
+            showMenu()
+            logoutEvent()
             return false
     }
-
-    $('ul.user_nav li.logout a').click(() => {
-        $.ajax({
-            url: backURL + 'user/logout',
-            xhrFields: {
-                withCredentials: true
-            },
-            success: function () {
-                localStorage.removeItem('loginedId')
-                location.href=frontURL + 'index.html'
-            }, error: function(xhr) {
-                alert(xhr.status)
-            }
+    //-- 로그아웃 이벤트 연결 START -- 
+    function logoutEvent() {
+        $('ul.user_nav li.logout a').click(() => {
+            console.log('머냐고')
+            $.ajax({
+                url: backURL + 'user/logout',
+                xhrFields: {
+                    withCredentials: true
+                },
+                success: function () {
+                    localStorage.removeItem('loginedId')
+                    location.href=frontURL + 'index.html'
+                }, error: function(xhr) {
+                    alert(xhr.status)
+                }
+            })
         })
-    })
+    }
+    //-- 로그아웃 이벤트 연결 END --
 })
