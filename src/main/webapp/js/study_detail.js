594ba152f195288fc2cfd853987d21187d229fd4
NYK <kny0339@gmail.com>
2022-12-23 15:24:17 +0900

    2022-12-23 김나영 스터디 상세, 거래내역 충돌 해결

--- src/main/webapp/js/study_detail.js
+++ src/main/webapp/js/study_detail.js
@@ -3,6 +3,7 @@ $(function() {
     let studyId =''
     let certification = ''
     let loginedId = localStorage.getItem('loginedId')
+    let studyFee = ''
     //-- 출석하기 요청을 위한 변수 세팅 END --
     let queryStr = location.search.substr(1).split('=')
     $.ajax({
@@ -15,19 +16,47 @@ $(function() {
             
             let study = jsonObj.study
 
+            //-- 상단 변수 세팅 START --
             studyId = study.studyId
             certification = study.studyCertification
-            let loginedUserHomeworkList = jsonObj.loginedStudyUser.homeworkList
-            if(jsonObj.loginedStudyUser.email == null) {
-                $('div.aside_atag').hide()
-            } else if(checkHomework(loginedUserHomeworkList)) {
-                $('div.aside_atag').show()
-                $('div.homework_btn').hide()
-                $('div.homework_done').show()
+            studyFee = study.studyFee
+            //-- 상단 변수 세팅 END --
+
+            if(jsonObj.loginedStudyUser != null) {
+                //-- 출석하기 버튼, 스터디 탈퇴 토글 START --
+                let loginedUserHomeworkList = jsonObj.loginedStudyUser.homeworkList
+                if(jsonObj.loginedStudyUser.email == null) {
+                    $('div.aside_atag').hide()
+                } else if(checkHomework(loginedUserHomeworkList)) {
+                    $('div.aside_atag').show()
+                    $('div.homework_btn').hide()
+                    $('div.homework_done').show()
+                } else if(!checkHomework(loginedUserHomeworkList)) {
+                    $('div.aside_atag').show()
+                    $('div.homework_btn').show()
+                    $('div.homework_done').hide()
+                }
+                //-- 출석하기 버튼 토글 END --
+    
+                //-- 스터디 탈퇴 버튼 토글 START --
+                if(jsonObj.loginedStudyUser.email == null) {
+                    $('div.leave_study').hide()
+                } else if(new Date() < new Date(study.studyStartDate)) {
+                    $('div.leave_study').show()
+                }
+                //-- 스터디 탈퇴 버튼 토글 END --
+            } else {
+                if(new Date() < new Date(study.studyStartDate)) {
+                    $('div.join_study').show()
+                } else {
+                    $('div.join_study').hide()
+                }
             }
-            $('section >div span').html(study.studyPostDate)
+
+            //-- 스터디 내용 세팅 START --
+            $('div.detail h2').html(study.studyTitle)
+            $('section.content_section h3').html(study.studyPostDate)
             
-            $('span.post_date').html()
             let subjectList =''
             $(study.subjects).each(function(index, s){
                 subjectList += s.subject.subjectName + ' '
@@ -49,6 +78,8 @@ $(function() {
             $('table> tbody>tr >th.leader').html(study.studyLeader.name)
             $('table> tbody>tr >td.diligence').html(study.studyLeader.diligence).append('점')
             $('table> tbody>tr >td.study_cnt').html(jsonObj.studyLeaderFinishStudy).append('회')
+            //-- 스터디 내용 세팅 END --
+
         }, error: function(xhr) {
             alert(xhr.status)
         }
@@ -59,12 +90,12 @@ $(function() {
         let today = new Date()
         let todayStr = today.getFullYear() + '/' + (today.getMonth() + 1) + '/' + today.getDate()
         let flag = false
-
         $.each(list, function(index, homework) {
             let submitDt = new Date(homework.studySubmitDt)
             let submitDtStr = submitDt.getFullYear() + '/' + (submitDt.getMonth() + 1) + '/' + submitDt.getDate()
-            if(new Date(todayStr).getTime() == new Date(submitDtStr).getTime()) {
+            if(new Date(todayStr).getTime() - new Date(submitDtStr).getTime() == 0) {
                 flag = true
+                return false
             }
         })
         return flag
@@ -73,7 +104,6 @@ $(function() {
 
     //-- 출석하기 버튼 클릭 START --
     $('a.homework_btn').click(() => {
-        
         if(loginedId != '' && loginedId != null) {
             let homeworkURL = ''
             switch(certification) {
@@ -95,7 +125,6 @@ $(function() {
                     location.reload()
                 }, error: function() {
                     alert('출석에 실패했습니다.')
-                    console.log()
                 }
             })
         } else {
@@ -104,4 +133,53 @@ $(function() {
         }
     })
     //-- 출석하기 버튼 클릭 END --
+
+    //-- 스터디 탈퇴하기 버튼 클릭 START --
+    $('a.leave_study_btn').click(() => {
+        
+    })
+    //-- 스터디 탈퇴하기 버튼 클릭 END --
+
+    //-- 스터디 참가신청 버튼 클릭 START --
+    $('a.join_study').click(() => {
+        $.ajax({
+            url: backURL + 'user/',
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(jsonObj) {
+                console.log(jsonObj)
+                if(jsonObj.userBalance < studyFee) {
+                    alert('지갑 잔액이 부족합니다.')
+                    location.href = frontURL + 'user_detail.html'
+                }
+                //--스터디 참여 ajax START --
+                let sendData = {}
+                sendData.studyFee = studyFee
+                sendData.studyId = studyId
+                sendData.studyCertification = certification
+                console.log(sendData)
+                $.ajax({
+                    url: backURL + 'study/join/' + studyId,
+                    method: 'post',
+                    data: JSON.stringify(sendData),
+                    contentType: 'application/json',
+                    xhrFields:{
+                        withCredentials: true
+                    },
+                    success: function() {
+                        alert('스터디 참여신청이 완료되었습니다.')
+                        location.reload()
+                    }, error: function(xhr) {
+                        alert(xhr.responseText)
+                    }
+                })
+                //--스터디 참여 ajax END --
+            }, error: function(xhr) {
+                alert('로그인 해주세요')
+                location.href = frontURL + 'index.html'
+            }
+        })
+    })
+    //-- 스터디 참가신청 버튼 클릭 END --
 })
